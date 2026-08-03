aa73c0cb50713aee0a1ebada3fe2bc744b55641d
Fabien <fabiendevfront@gmail.com>
2026-08-03 11:15:56 +0200

    refactor: optimize DOM queries in navbar.js

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,14 +1,12 @@
 // Toggle mobile menu
 export const initNavbar = () => {
   const menuToggle = document.querySelector(".menu-toggle");
-  if (!menuToggle) return;
+  const navbar = document.querySelector("nav");
+  const iconOpen = document.querySelector(".icon-open");
+  const iconClose = document.querySelector(".icon-close");
+  const menuLink = document.querySelectorAll("nav a");
+  if (!menuToggle || !navbar || !iconOpen || !iconClose) return;
   menuToggle.addEventListener("click", () => {
-    const navbar = document.querySelector("nav");
-    const iconOpen = document.querySelector(".icon-open");
-    const iconClose = document.querySelector(".icon-close");
-    if (!navbar) return;
-    if (!iconOpen) return;
-    if (!iconClose) return;
     navbar.classList.toggle("hidden");
     navbar.classList.toggle("flex");
     menuToggle.setAttribute(
@@ -18,12 +16,12 @@ export const initNavbar = () => {
     iconOpen.classList.toggle("hidden");
     iconClose.classList.toggle("hidden");
   });
-  const menuLink = document.querySelectorAll("nav a");
+
   if (menuLink.length === 0) return;
   menuLink.forEach((link) => {
     link.addEventListener("click", () => {
-      document.querySelector("nav").classList.add("hidden");
-      document.querySelector("nav").classList.remove("flex");
+      navbar.classList.add("hidden");
+      navbar.classList.remove("flex");
       menuToggle.setAttribute("aria-expanded", "false");
     });
   });
